4a22d1c09dcefc25d557f00f1fbbd080e252bf21
Felipe Campos <fmrcampos@berkeley.edu>
2021-01-27 19:29:47 -0500

    velocity agent setup fixes

--- gym-swarm-sim/envs/swarmsimmaster/core/velo_controlled_agent.py
+++ gym-swarm-sim/envs/swarmsimmaster/core/velo_controlled_agent.py
@@ -17,12 +17,13 @@ class VeloAgent(agent.Agent):
 
     #TODO: Refactor with the parent class to remove the code written twice.
     def move(self):
-        #check to make sure that this doesnt throw an error and conforms to grid types.
-        direction_coord =   tuple(np.add(self.velocities, self.coordinates))
+        # check to make sure that this doesnt throw an error and conforms to grid types.
+        direction_coord = tuple(np.add(self.velocities, self.coordinates))
         direction_coord = self.check_within_border(self.velocities, direction_coord)
+        print(f"valid coords: {self.world.grid.are_valid_coordinates(direction_coord)} {direction_coord not in self.world.agent_map_coordinates} {self._Agent__isCarried}")
         if self.world.grid.are_valid_coordinates(direction_coord) \
                 and direction_coord not in self.world.agent_map_coordinates \
-                and not self._Agent__isCarried:#this is a little jank IK
+                and not self._Agent__isCarried: #this is a little jank IK
             if self.coordinates in self.world.agent_map_coordinates:
                 del self.world.agent_map_coordinates[self.coordinates]
             self.coordinates = direction_coord
