c3f7ce26a875b65341dcf5364d6cf1ce4bfa781d
MarkSelden <seldenm@berkeley.edu>
2021-02-17 23:24:50 -0800

    added spacing configuration

--- gym-swarm-sim/envs/swarmsimmaster/components/scenario/lonely_agent.py
+++ gym-swarm-sim/envs/swarmsimmaster/components/scenario/lonely_agent.py
@@ -8,28 +8,30 @@ SPACING = 3.0
 def scenario(world, init_scenario="edge_line_flock"):
     # np.random.rand(world.seed) # TODO: get this from config somehow? configdata param or something
 
+    spacing = world.config_data.spacing
+    print(f"spacing {spacing}")
     if init_scenario == "center_radius_flock":
         world.add_agent((0.0, 0.0))
         for i in range(NUM_AGENTS):
-            x, y = SPACING * NUM_AGENTS * (np.random.rand(2) - 1)
+            x, y = spacing * NUM_AGENTS * (np.random.rand(2) - 1)
             world.add_agent((x, y))
     elif init_scenario == "center_line_flock":
         world.add_agent((0.0, 0.0))
         for i in range(1,12,2):
             epsilon = (np.random.rand() - .5) / 2
-            world.add_agent((0.0, SPACING * float(i) + epsilon))
+            world.add_agent((0.0, spacing * float(i) + epsilon))
 
         for i in range(1,12,2):
             epsilon = (np.random.rand() - .5) / 2
-            world.add_agent((0.0, - SPACING * float(i) + epsilon))
+            world.add_agent((0.0, - spacing * float(i) + epsilon))
     elif init_scenario == "edge_line_flock":
-        world.add_agent((0.0, SPACING * float(NUM_AGENTS // 2)))
+        world.add_agent((0.0, spacing * float(NUM_AGENTS // 2)))
         for i in range(0, NUM_AGENTS // 2 - 1,2):
             epsilon = (np.random.rand() - .5) / 2
-            world.add_agent((0.0, SPACING * float(i) + epsilon))
+            world.add_agent((0.0, spacing * float(i) + epsilon))
 
         for i in range(1, NUM_AGENTS // 2,2):
             epsilon = (np.random.rand() - .5) / 2
-            world.add_agent((0.0, -SPACING * float(i) + epsilon))
+            world.add_agent((0.0, -spacing * float(i) + epsilon))
 
     # if radius: monte carlo! TODO
