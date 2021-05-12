4e0ec4e96fc09bac717d5d17e6cb4014ac9fa469
MarkSelden <seldenm@berkeley.edu>
2021-05-12 11:48:39 -0700

    dual vis

---
+++ swarmsimmaster/components/scenario/dual_vis_tisch.py
@@ -0,0 +1,40 @@
+import rpyc
+
+
+def scenario(world):
+    world.interface_server = rpyc.connect("localhost", 18861, config={'allow_public_attrs': True, 'allow_all_attrs': True, 'allow_pickle': True}).root
+    while not world.interface_server.simulation_ready():
+        continue
+    print('simulation ready')
+    args, kwargs = world.interface_server.get_initialization_args()
+    print(args, kwargs)
+    net_config = args[0]
+
+    world.config_data.follow_the_leader = net_config['follow']
+    world.config_data.flock_rad = net_config['flock_rad']
+    world.config_data.flock_vel = net_config['flock_vel']
+    goons = kwargs['goons']
+    world.timestep = kwargs['timestep']
+# These are for logging which we will get to
+  # seed = kwargs['seed']
+  # update_period = self.kwargs['update_period']
+    if goons:
+        for agent in goons:
+            world.add_agent(agent)
+    else:
+        world.add_agent(world.grid.get_center())
+
+
+
+    id_map = world.get_agent_map_id()
+    positions = {}
+    for agent_id in id_map:
+        mote = id_map[agent_id]
+        positions[agent_id] = mote.coordinates
+
+    world.interface_server.update_mote_states(positions)
+
+    world.interface_server.simulation_initialized()
+
+
+
---
+++ swarmsimmaster/components/solution/tisch_visualization.py
@@ -0,0 +1,43 @@
+import random
+import time
+
+def solution(world):
+    #wait for command to run the main loop
+    while not world.interface_server.should_loop():
+        continue
+
+
+    #set neighbors
+    if world.interface_server.should_set_neighbors():
+        id_map = world.get_agent_map_id()
+        agent_neighbor_table = world.interface_server.get_neighbors_table()
+        mote_key_map = world.interface_server.get_mote_key_map()
+        for (net_id, neighbors) in agent_neighbor_table:
+            agent_id = mote_key_map[net_id]  # NOTE: this is set in the network simulator
+            mote = id_map[agent_id]
+            mote.id = agent_id  # FIXME: THIS IS DUMB BUT IT WORKS
+            mote.neighbors = neighbors
+
+            mote.control_update(mote_key_map, {v : k for (k, v) in mote_key_map.items()})
+
+    #assign velocities
+    if world.interface_server.should_assign_velos():
+        id_map = world.get_agent_map_id()
+        new_velos = world.interface_server.get_new_velocities()
+        for mote in new_velos:
+            id_map[mote].set_velocities(new_velos[mote])
+
+
+    #move agents
+    for agent in world.get_agent_list():
+        agent.move()
+
+    #update RPC on mote states
+    id_map = world.get_agent_map_id()
+    positions = {}
+    for agent_id in id_map:
+        mote = id_map[agent_id]
+        positions[agent_id] = mote.coordinates
+
+    world.interface_server.update_mote_states(positions)
+
